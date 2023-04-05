a26719aaa76633db8e5a741e92b9ba6ce61c5172
DanielSP99 <daniel.sanchez@ingenierosmafur.com>
2023-04-05 13:32:26 -0600

    Initial Commit

---
+++ PokemonGG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonGG
+//
+//  Created by Daniel Sanchez Peraza on 05/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
